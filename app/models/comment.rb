24a2e15b2094560b18c4764d7a75c9adc5f7d63c
emerson <emerson.almeida@a.ficr.com.br>
2022-09-15 17:34:30 -0300

    estilizando application.html.erb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
+  validates :author, :body, presence:true
+
+  #relacionamento com o post
   belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ApplicationRecord
+    # validaçoes de campos
+    validates :title, :author, :body, presence:true
+
+
+    has_many :comments
 end
